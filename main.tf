5c7162c6fea810bca166b5392fbad81e4db0137a
Integralist <mark.mcdx@gmail.com>
2023-07-04 11:29:34 +0100

    configuration files and ci

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    fastly = {
+      source = "fastly/fastly"
+      version = "5.2.2"
+    }
+  }
+}
+
+resource "fastly_service_vcl" "demo" {
+  name = "terraform-github-actions-testing"
+
+  domain {
+    name    = "terraform-github-actions-testing.integralist.co.uk"
+  }
+
+  backend {
+    address = "127.0.0.1"
+    name    = "localhost"
+    port    = 80
+  }
+
+  force_destroy = true
+}
