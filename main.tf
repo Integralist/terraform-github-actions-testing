d0464c1b12335cf225d6f92ca048f7874c6540bc
Integralist <mark.mcdx@gmail.com>
2023-07-04 11:36:50 +0100

    delete service

--- main.tf
+++ main.tf
@@ -7,18 +7,18 @@ terraform {
   }
 }
 
-resource "fastly_service_vcl" "demo" {
-  name = "terraform-github-actions-testing"
-
-  domain {
-    name = "terraform-github-actions-testing.integralist-testing.co.uk"
-  }
-
-  backend {
-    address = "127.0.0.1"
-    name    = "localhost"
-    port    = 80
-  }
-
-  force_destroy = true
-}
+# resource "fastly_service_vcl" "demo" {
+#   name = "terraform-github-actions-testing"
+#
+#   domain {
+#     name = "terraform-github-actions-testing.integralist-testing.co.uk"
+#   }
+#
+#   backend {
+#     address = "127.0.0.1"
+#     name    = "localhost"
+#     port    = 80
+#   }
+#
+#   force_destroy = true
+# }
