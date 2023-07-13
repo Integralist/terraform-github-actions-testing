3233f8f1ed607c74d0c4b7b0974b305cc65b388a
Integralist <mark.mcdx@gmail.com>
2023-07-13 15:05:34 +0100

    delete service

--- modules/service-vcl/main.tf
+++ modules/service-vcl/main.tf
@@ -7,18 +7,18 @@ terraform {
   }
 }
 
-resource "fastly_service_vcl" "demo" {
-  name = "testing-multiple-environments"
-
-  domain {
-    name = "testing-multiple-environments.integralist-new.co.uk"
-  }
-
-  backend {
-    address = "127.0.0.1"
-    name    = "localhost"
-    port    = 80
-  }
-
-  force_destroy = true
-}
+# resource "fastly_service_vcl" "demo" {
+#   name = "testing-multiple-environments"
+#
+#   domain {
+#     name = "testing-multiple-environments.integralist-new.co.uk"
+#   }
+#
+#   backend {
+#     address = "127.0.0.1"
+#     name    = "localhost"
+#     port    = 80
+#   }
+#
+#   force_destroy = true
+# }
