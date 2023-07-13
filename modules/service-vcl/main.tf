8e89bf560d33231d0e604b1c2dd626101f076689
Integralist <mark.mcdx@gmail.com>
2023-07-13 14:54:04 +0100

    new structure

---
+++ modules/service-vcl/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    fastly = {
+      source  = "fastly/fastly"
+      version = "5.2.2"
+    }
+  }
+}
+
+resource "fastly_service_vcl" "demo" {
+  name = "testing-multiple-environments"
+
+  domain {
+    name = "testing-multiple-environments.integralist.co.uk"
+  }
+
+  backend {
+    address = "127.0.0.1"
+    name    = "localhost"
+    port    = 80
+  }
+
+  force_destroy = true
+}
